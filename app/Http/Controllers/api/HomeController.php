dea33130d37f5f15005adc1e04aad399bfed7d06
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-11 10:18:35 +0000

    add notation de homecontroller

--- app/Http/Controllers/api/HomeController.php
+++ app/Http/Controllers/api/HomeController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+/**
+* @OA\Post(
+     *     path="/api/home",
+     *     summary="Home data",
+     *     description="",
+     *     tags={"Home"},
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="Provide your name",
+     *         required=true,
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK",
+     *         @OA\MediaType(
+     *             mediatype="application/json",
+     *)
+     * ),
+     *     
+     * )
+     */
 class HomeController extends Controller
 {
     //
